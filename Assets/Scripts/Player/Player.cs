57d7bc1b9a2ce5f464ca4a8966e87415eb0db604
William Johnson <johnsonalex1@gatech.edu>
2014-11-10 13:53:18 -0500

    Init second repo

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour {
+
+	public List<Item> inventory = new List<Item>();
+	private Weapon activeWeapon;
+
+	// Use this for initialization
+	void Start () {
+		activeWeapon = (Weapon)inventory[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void Attack () {
+		GameObject attack = activeWeapon.attackOBJ;
+		Instantiate(attack, transform.position, transform.localRotation);
+	}
+}
